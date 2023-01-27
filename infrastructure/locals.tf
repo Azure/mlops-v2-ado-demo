61e90d89ad3bf35b2c36b8e41e18715514d6002e
Setu Chokshi <setuc@users.noreply.github.com>
2023-01-27 15:25:13 +0800

    Updated files as per Samer's original fixes

--- infrastructure/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  tags = {
-    Owner       = "mlops-v2"
-    Project     = "mlops-v2"
-    Environment = "${var.environment}"
-    Toolkit     = "terraform"
-    Name        = "${var.prefix}"
-  }
-}
